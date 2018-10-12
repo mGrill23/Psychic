60afecbff32c009fdd803798f5aae8513cead862
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2018-10-12 12:39:58 -0400

    AddingBasicGameLogic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,5 +11,23 @@ var lossesNum = document.getElementById("losses-count");
 document.onkeyup = function(event) {
     var randomNum = Math.floor(Math.random() * compChoices.length);
     var compChoice = compChoices[randomNum];
+
+    var userGuess = event.key;
+
+    if (userGuess === compChoice){
+        wins++;
+        guesses = 15;
+    }
+
+    else if (userGuess != compChoice && guesses == 0) {
+        losses++;
+        guesses = 15;
+    }
+
+    else{
+        guesses--;
+    }
+
+
 }
 
