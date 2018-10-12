8babc9a67c88018c6efb33c1a701be72c1378d4f
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2018-10-12 12:19:49 -0400

    InitialHtmlAndJavascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var wins = 0;
+var losses = 0;
+var guesses = 15;
+
+var compChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var winsNum = document.getElementById("wins-count");
+var guessNum = document.getElementById("guess-count");
+var lossesNum = document.getElementById("losses-count");
+
+document.onkeyup = function(event) {
+    var randomNum = Math.floor(Math.random() * compChoices.length);
+    var compChoice = compChoices[randomNum];
+}
+
